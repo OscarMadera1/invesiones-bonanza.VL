84e6c9270a81b55044c9d61e20c15bfaaecd47d9
oscar <omaderanegrete@gmail.com>
2025-05-28 19:58:22 -0500

    agregando los modelos al admin.py y agregando codeudor al forms.py en el paquete cliente.

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ZonaCobro,Cliente
 
-# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(ZonaCobro)
+
--- clientes/forms.py
+++ clientes/forms.py
@@ -4,12 +4,13 @@ from .models import Cliente
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
-        fields = ['identificacion','nombre' ,'apellido', 'telefono', 'direccion', 'zona', 'municipio']
+        fields = ['identificacion','nombre' ,'apellido', 'telefono','codeudor', 'direccion', 'zona', 'municipio']
         widgets = {
             'nombre': forms.TextInput(attrs={'class': 'form-control'}),
             'apellido': forms.TextInput(attrs={'class': 'form-control'}),
             'identificacion': forms.TextInput(attrs={'class': 'form-control'}),
             'telefono': forms.TextInput(attrs={'class': 'form-control'}),
+            'codeudor': forms.TextInput(attrs={'class': 'form-control'}),
             'direccion': forms.TextInput(attrs={'class': 'form-control'}),
             'zona': forms.Select(attrs={'class': 'form-select'}),
             'municipio': forms.Select(attrs={'class': 'form-select'}),
